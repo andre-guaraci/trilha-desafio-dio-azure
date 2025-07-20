dd0c5043e71124be17fc7818ecdeab630fc5235e
André Guaraci <guaraci.goncalves81@outlook.com>
2025-07-20 14:01:11 -0300

    Correção do endereçamento da pasta home
    Correção do endereçamento da pasta home

---
+++ CadastroFuncionarios/Views/Funcionarios/Error.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CadastroFuncionarios.Views.Funcionarios
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
